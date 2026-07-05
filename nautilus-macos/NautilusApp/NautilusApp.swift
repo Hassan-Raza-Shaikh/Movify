399b93365c85d45052c601f572c86b4b8c04f813
Hassan-Raza-Shaikh <ammarbatman9@gmail.com>
2026-07-06 00:10:01 +0500

    chore: decouple Python process management from SwiftUI client app and optimize for standalone server

--- nautilus-macos/NautilusApp/NautilusApp.swift
+++ nautilus-macos/NautilusApp/NautilusApp.swift
@@ -1,14 +1,6 @@
 import SwiftUI
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationWillFinishLaunching(_ notification: Notification) {
-        ProcessManager.shared.startBackend()
-    }
-    
-    func applicationWillTerminate(_ notification: Notification) {
-        ProcessManager.shared.stopBackend()
-    }
-    
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }

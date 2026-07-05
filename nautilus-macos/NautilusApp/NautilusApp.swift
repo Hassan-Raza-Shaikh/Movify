3ff06f4910adde2ed619fabbbcf6e6bee1c35c65
Hassan-Raza-Shaikh <ammarbatman9@gmail.com>
2026-07-06 00:16:03 +0500

    chore: restore automated server lifecycle in AppDelegate under native Movify app bundle

--- nautilus-macos/NautilusApp/NautilusApp.swift
+++ nautilus-macos/NautilusApp/NautilusApp.swift
@@ -1,6 +1,14 @@
 import SwiftUI
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        ProcessManager.shared.startBackend()
+    }
+    
+    func applicationWillTerminate(_ notification: Notification) {
+        ProcessManager.shared.stopBackend()
+    }
+    
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
